ea03125043e9b8f914ed8eac1c17cb310e1b45eb
Mobarak52 <mobarakhossain3552@gmail.com>
2025-10-29 14:16:37 +0600

    23-54427-3

---
+++ Task1/Task1/Program.cs
@@ -0,0 +1,8 @@
+﻿    Console.Write("Enter score of student  1 : ");
+    double score1 = Convert .ToDouble(Console.ReadLine());
+        Console.Write("Enter score of student  1 : ");
+    double score2 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Enter score of student  1 : ");
+    double score3 = Convert.ToDouble(Console.ReadLine());
+    double average = (score1 + score2 + score3) / 3;
+    Console.WriteLine($"Averaqge score : {average}");
---
+++ Task2/Task2/Program.cs
@@ -0,0 +1,5 @@
+﻿double price = 250;
+double discount = 0.15;
+double finalPrice = price - (price * discount);
+Console.WriteLine($"This is the final price : {finalPrice}");
+
